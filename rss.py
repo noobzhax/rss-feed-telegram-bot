0b3c3c4d6def388d72f9b135484d8f5840d9e869
Sourcery AI <>
2023-07-02 06:35:42 +0000

    'Refactored by Sourcery'

--- rss.py
+++ rss.py
@@ -16,7 +16,7 @@ if os.path.exists("config.env"):
 try:
     api_id = int(os.environ.get("API_ID"))   # Get it from my.telegram.org
     api_hash = os.environ.get("API_HASH")   # Get it from my.telegram.org
-    feed_urls = list(set(i for i in os.environ.get("FEED_URLS").split("|")))  # RSS Feed URL of the site.
+    feed_urls = list(set(os.environ.get("FEED_URLS").split("|")))
     bot_token = os.environ.get("BOT_TOKEN")   # Get it by creating a bot on https://t.me/botfather
     log_channel = int(os.environ.get("LOG_CHANNEL"))   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
     check_interval = int(os.environ.get("INTERVAL", 10))   # Check Interval in seconds.  
@@ -28,7 +28,7 @@ except Exception as e:
 
 
 for feed_url in feed_urls:
-    if db.get_link(feed_url) == None:
+    if db.get_link(feed_url) is None:
         db.update_link(feed_url, "*")
 
 
